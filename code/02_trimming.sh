8f74d0f63dd5e8b95fa4371696fe603e1d04372a
Fredrik Levin <fredrik@levingotland.se>
2020-04-03 16:20:11 +0200

    Added a script for the trimming

---
+++ code/02_trimming.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:45:00
+#SBATCH -J trimming_of_RNAreads
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user fredrik@levingotland.se
+# This script will trim the paired RNA-reads using Trimmomatic. 
+
+# Loading the required modules
+module load bioinfo-tools
+module load trimmomatic
+
+# Running Trimmomatic on the RNA-reads from the two different sites.
+trimmomatic PE -threads 2 -phred33 -trimlog /home/frle5739/genome_analysis/analyses/02_preprocessing/trimlog_SRR4342137 \
+/home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342137.1.fastq.gz \
+/home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342137.2.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_paired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_unpaired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_paired_reverse.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_unpaired_forward.fastq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
+
+trimmomatic PE -threads 2 -phred33 -trimlog /home/frle5739/genome_analysis/analyses/02_preprocessing/trimlog_SRR4342139 \
+/home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342139.1.fastq.gz \
+/home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342139.2.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_paired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_unpaired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_paired_reverse.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_unpaired_forward.fastq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
