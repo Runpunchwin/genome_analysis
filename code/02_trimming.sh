ccf16b5392bf531ad0105fd5e24a7f118d7bd9cb
Fredrik Levin <fredrik@levingotland.se>
2020-04-15 10:17:01 +0200

    modified the trimming script

--- code/02_trimming.sh
+++ code/02_trimming.sh
@@ -13,20 +13,20 @@ module load bioinfo-tools
 module load trimmomatic
 
 # Running Trimmomatic on the RNA-reads from the two different sites.
-trimmomatic PE -threads 2 -phred33 -trimlog /home/frle5739/genome_analysis/analyses/02_preprocessing/trimlog_SRR4342137 \
+trimmomatic PE -threads 2 -phred33 \
 /home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342137.1.fastq.gz \
 /home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342137.2.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_paired_forward.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_unpaired_forward.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_paired_reverse.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342137_unpaired_reverse.fastq.gz \
-ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342137_paired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342137_unpaired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342137_paired_reverse.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342137_unpaired_reverse.fastq.gz \
+ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
 
-trimmomatic PE -threads 2 -phred33 -trimlog /home/frle5739/genome_analysis/analyses/02_preprocessing/trimlog_SRR4342139 \
+trimmomatic PE -threads 2 -phred33 \
 /home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342139.1.fastq.gz \
 /home/frle5739/genome_analysis/data/raw_data/RNA_untrimmed/SRR4342139.2.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_paired_forward.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_unpaired_forward.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_paired_reverse.fastq.gz \
-/home/frle5739/genome_analysis/data/trimmed_data/SRR4342139_unpaired_reverse.fastq.gz \
-ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342139_paired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342139_unpaired_forward.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342139_paired_reverse.fastq.gz \
+/home/frle5739/genome_analysis/data/trimmed_data/RNA_trimmed/SRR4342139_unpaired_reverse.fastq.gz \
+ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:50
