ce321600f8f5d2f843b5db4912f456ece88336bc
Fredrik Levin <fredrik@levingotland.se>
2020-04-15 14:45:20 +0200

    Assembly- and multiqc-script added

---
+++ code/01_3_multiQC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:20:00
+#SBATCH -J multiQC
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user fredrik@levingotland.se
+# This script will summarize the Quality Control (QC) performed on the reads
+
+# Loading the required modules
+module load bioinfo-tools
+module load MultiQC/1.8
+
+# Running MultiQC on all QC-results for DNA- and RNA-reads
+multiqc -o /home/frle5739/genome_analysis/analyses/01_QC_reads/multiqc /home/frle5739/genome_analysis/analyses/01_QC_reads/DNA_trimmed_QC
+
+multiqc -o /home/frle5739/genome_analysis/analyses/01_QC_reads/multiqc /home/frle5739/genome_analysis/analyses/01_QC_reads/RNA_trimmed_QC2
+
+multiqc -o /home/frle5739/genome_analysis/analyses/01_QC_reads/multiqc /home/frle5739/genome_analysis/analyses/01_QC_reads/RNA_untrimmed_QC
---
+++ code/03_assembly.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 07:00:00
+#SBATCH -J DNA_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user fredrik@levingotland.se
+# This script will perform a metagenomic assembly of DNA-reads. 
+
+# Loading the required modules
+module load bioinfo-tools
+module load megahit
+
+# Performing the assembly using Megahit
+megahit -t 2 --kmin-1pass --k-min 65 --k-max 105 --k-step 10 \
+-1 /home/frle5739/genome_analysis/data/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,/home/frle5739/genome_analysis/data/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz \
+-2 /home/frle5739/genome_analysis/data/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,/home/frle5739/genome_analysis/data/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz \
+-o /home/frle5739/genome_analysis/data/DNA_assembly
