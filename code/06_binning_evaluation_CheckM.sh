a57142949196b22495a1f9e028aa21ae0d716b35
Fredrik Levin <fredrik@levingotland.se>
2020-04-22 21:49:52 +0200

    Fixed errors in the binning evaluation

--- code/06_binning_evaluation_CheckM.sh
+++ code/06_binning_evaluation_CheckM.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -A g2020008
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 02:00:00
-#SBATCH -J assembly_evaluation
+#SBATCH -n 4
+#SBATCH -t 04:00:00
+#SBATCH -J binning_evaluation
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user fredrik@levingotland.se
 # This script will perform an evaluation of the binning 
@@ -13,4 +13,4 @@ module load bioinfo-tools
 module load CheckM/1.0.12
 
 # Performing the evaluation using CheckM
-
+checkm lineage_wf -t 4 -x fa --reduced_tree /home/frle5739/genome_analysis/data/bins /home/frle5739/genome_analysis/06_binning_evaluation/checkm
--- code/06_binning_evaluation_MetaQuast.sh
+++ code/06_binning_evaluation_MetaQuast.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 02:00:00
-#SBATCH -J assembly_evaluation
+#SBATCH -J binning_evaluation
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user fredrik@levingotland.se
 # This script will perform an evaluation of the binning 
