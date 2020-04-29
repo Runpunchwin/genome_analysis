f6e8daa7130cd0e2636d6d22711a4857242b4c0e
Fredrik Levin <fredrik@levingotland.se>
2020-04-29 14:40:21 +0200

    Script for annotation

--- code/07_annotation.sh
+++ code/07_annotation.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 04:00:00
+#SBATCH -t 02:00:00
 #SBATCH -J annotation
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user fredrik@levingotland.se
@@ -10,8 +10,18 @@
 
 # Loading the required modules
 module load bioinfo-tools
-module load 
+module load prokka/1.12-12547ca
 
 # Performing the annotation using Prokka
-
-
+# Loops over the input bins and uses the filepath to create a prefix for the files
+for f in ~/genome_analysis/data/bins/*.fa; 
+do i="${f%.fa}";
+j="${i//data\/bins\//analyses/07_annotation/}"; 
+k="${i///home\/frle5739\/genome_analysis\/data\/bins\//}";
+if [[ $j == bin_1 || $j == bin_2 || $j == bin_3 || $j == bin_21 ]]
+then 
+prokka --outdir $j --kingdom Archea --prefix $k $i
+else
+prokka --outdir $j --prefix $k $i
+fi; 
+done
