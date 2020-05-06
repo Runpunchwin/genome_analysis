0cc3cf8d96e474f6bdf015478dfc128d9b1165f3
Fredrik Levin <fredrik@levingotland.se>
2020-05-06 16:13:52 +0200

    Finished the BWA-script

--- code/08_mapping.sh
+++ code/08_mapping.sh
@@ -11,7 +11,30 @@
 # Loading the required modules
 module load bioinfo-tools
 module load bwa/0.7.17
+module load samtools/1.10
 
 # Performing the mapping using bwa
+# Saving the path to the RNA data
+$RNAreads = ~/genome_analysis/data/trimmed_data/RNA_trimmed_modified
 
+# Looping through the bins
+for bin in ~/genome_analysis/data/bins/*.fa
+do
+bwa index $bin
+# Saving the bin-number as a variable (e.g. bin_3)
+binNo="${bin///home\/frle5739\/genome_analysis\/data\/bins\//}"
+binNo="${binNo//.fa/}"
 
+bwa mem -t 2 $bin $RNAreads/SRR4342137_paired_forward.fastq.gz $RNAreads/SRR4342137_paired_reverse.fastq.gz > ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.sam
+bwa mem -t 2 $bin $RNAreads/SRR4342139_paired_forward.fastq.gz $RNAreads/SRR4342139_paired_reverse.fastq.gz > ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.sam
+
+# Piping the samtools-commands in order to avoid large temporary files
+samtools view -b ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.sam |
+ samtools sort > ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.bam
+samtools view -b ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.sam|
+ samtools sort -@ 2 > ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.bam
+
+# Remove the large, uncompressed files
+rm ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.sam
+rm ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.sam
+done
