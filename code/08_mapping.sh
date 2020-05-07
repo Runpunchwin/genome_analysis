bf7b19ff00c504cf80da03c136d6e36d1bee4a83
Fredrik Levin <fredrik@levingotland.se>
2020-05-07 10:55:14 +0200

    Small visual changes in the code

--- code/08_mapping.sh
+++ code/08_mapping.sh
@@ -30,8 +30,8 @@ bwa mem -t 2 $bin $RNAreads/SRR4342139_paired_forward.fastq.gz $RNAreads/SRR4342
 
 # Piping the samtools-commands in order to avoid large temporary files
 samtools view -b ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.sam |
- samtools sort > ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.bam
-samtools view -b ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.sam|
+ samtools sort -@ 2 > ~/genome_analysis/data/mapped_RNA/SRR4342137/${binNo}_mapped_RNA_SRR4342137.bam
+samtools view -b ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.sam |
  samtools sort -@ 2 > ~/genome_analysis/data/mapped_RNA/SRR4342139/${binNo}_mapped_RNA_SRR4342139.bam
 
 # Remove the large, uncompressed files
