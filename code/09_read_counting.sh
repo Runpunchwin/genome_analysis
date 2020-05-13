47bd403004f90d2b47e221bf9ab363d0fcab032c
Fredrik Levin <fredrik@levingotland.se>
2020-05-13 12:12:06 +0200

    Finished the script for read counting

--- code/09_read_counting.sh
+++ code/09_read_counting.sh
@@ -12,13 +12,19 @@
 module load bioinfo-tools
 module load htseq/0.9.1
 
+# Saving the filepaths as variables
+binpath37="/home/frle5739/genome_analysis/data/mapped_RNA/SRR4342137"
+binpath39="/home/frle5739/genome_analysis/data/mapped_RNA/SRR4342139"
+prokkaPath="/home/frle5739/genome_analysis/analyses/07_annotation"
+
 # Performing the read count using htseq
-for bin in ~/genome_analysis/data/mapped_RNA/SRR4342137/*
+# Only looking at bins with contamination <10%
+for bin in bin_2 bin_4 bin_5 bin_6 bin_8 bin_9 bin_10 bin_11 bin_13 bin_14 bin_15 bin_16 bin_17 bin_18 bin_21 bin_22 bin_24 bin_26
 do
-htseq-count -f bam -r pos -t CDS -i ID 
+htseq-count -f bam -r pos -t CDS -i ID $binpath37/${bin}_mapped_RNA_SRR4342137.bam <(sed '/##FASTA/$d' $prokkaPath/${bin}/${bin}.gff) > /home/frle5739/genome_analysis/analyses/09_read_counting/SRR4342137/${bin}_count.out
 done
 
-for bin in ~/genome_analysis/data/mapped_RNA/SRR4342139/*
+for bin in bin_2 bin_4 bin_5 bin_6 bin_8 bin_9 bin_10 bin_11 bin_13 bin_14 bin_15 bin_16 bin_17 bin_18 bin_21 bin_22 bin_24 bin_26
 do
-htseq-count -f bam -r pos -t CDS -i ID
+htseq-count -f bam -r pos -t CDS -i ID $binpath39/${bin}_mapped_RNA_SRR4342137.bam <(sed '/##FASTA/$d' $prokkaPath/${bin}/${bin}.gff) > /home/frle5739/genome_analysis/analyses/09_read_counting/SRR4342139/${bin}_count.out
 done
