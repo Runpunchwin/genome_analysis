e94f328a35fc860f01fe9190ce56393de64b0edb
Fredrik Levin <fredrik@levingotland.se>
2020-05-20 12:45:09 +0200

    testing rpkm

--- code/bash_commands/RPKM.py
+++ code/bash_commands/RPKM.py
@@ -10,21 +10,22 @@ i = 0
 sumReads = 0
 
 for line in count_file:
-    words = line.split("\t")
-    sumReads += float(words[1])
-    if re.match(r'^__', words[0]):
-        continue
-    else:
-        for line2 in map_file:
-            if re.match(r'^##', line2):
-                continue
-            else:
-                split1 = line2.split("ID=")
-                if split1[1] == words[0]:
-                    split2 = split1[1].split("\t")
-                    genelengths[i, 0] = (float(words[1]))
-                    genelengths[i, 1] = (float(split2[4])-float(split2[3]))
-                    i += 1
+    if line not in ["\n", "\r\n"]:
+        words = line.split("\t")
+        sumReads += float(words[1])
+        if re.match(r'^__', words[0]):
+            continue
+        else:
+            for line2 in map_file:
+                if re.match(r'^##', line2):
+                    continue
+                else:
+                    split1 = line2.split("ID=")
+                    if split1[1] == words[0]:
+                        split2 = split1[1].split("\t")
+                        genelengths[i, 0] = (float(words[1]))
+                        genelengths[i, 1] = (float(split2[4])-float(split2[3]))
+                        i += 1
 
 
 def rpkm(geneReads, geneLength, totReads):
