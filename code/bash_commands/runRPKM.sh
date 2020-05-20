0b56216cb5c82f04ccd2da38858c69dd4d30be22
Fredrik Levin <fredrik@levingotland.se>
2020-05-20 14:20:03 +0200

    testing rpkm

--- code/bash_commands/runRPKM.sh
+++ code/bash_commands/runRPKM.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J rpkm
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user fredrik@levingotland.se
 
 python rpkm.py /home/frle5739/genome_analysis/analyses/09_read_counting/SRR4342139/bin_22_count.out /home/frle5739/genome_analysis/analyses/07_annotation/bin_22/bin_22.gff >> /home/frle5739/genome_analysis/analyses/09_read_counting/SRR4342139/normalised/bin_22_normalised.out
